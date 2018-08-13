3004b5dfad492e27162c54ead2dc15d2ea03009f
Randy Weyer <randy@>
2018-08-13 09:51:43 -0700

    Initial Commit

---
+++ app/to-do.js
@@ -0,0 +1,19 @@
+var Task = /** @class */ (function () {
+    function Task(descriptionParameter, priorityParameter) {
+        this.descriptionParameter = descriptionParameter;
+        this.priorityParameter = priorityParameter;
+    }
+    Task.prototype.markDone = function () {
+        this.done = true;
+    };
+    return Task;
+}());
+var tasks = [];
+tasks.push(new Task('Do the dishes.', 'Medium'));
+tasks.push(new Task('Buy chocolate.', 'Low'));
+tasks.push(new Task('Do laundry.', 'High'));
+tasks[0].markDone();
+for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
+    var task = tasks_1[_i];
+    console.log(task);
+}
